94e6d06d2975e2a0070edf797c68fddd945a1f17
Sam Skulsky <sam_skulsky@icloud.com>
2023-10-18 18:54:48 -0700

    emotes, scrolling

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
 "use client";
 
 import { useEffect, useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { db, auth, googleAuthProvider } from "./firebaseClient";
 import { signInWithPopup, signOut as signOutFromFirebase } from "firebase/auth";
 import {
   collection,
   addDoc,
+  setDoc,
   onSnapshot,
   query,
   orderBy,
   serverTimestamp,
+  updateDoc,
+  doc,
 } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 
@@ -18,10 +22,13 @@ export default function Home() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const messagesEndRef = useRef(null);
+  const [prevMessageCount, setPrevMessageCount] = useState(0);
+
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const chatInputRef = useRef(null);
 
   useEffect(() => {
     if (auth) {
-      // Authentication listener
       onAuthStateChanged(auth, (user) => {
         if (user) {
           setUser(user);
@@ -29,12 +36,10 @@ export default function Home() {
           const messagesCollection = collection(db, "messages");
           const q = query(messagesCollection, orderBy("createdAt"));
 
-          // Listen for new messages
           const unsubscribe = onSnapshot(q, (snapshot) => {
             setMessages(
               snapshot.docs.map((doc) => {
                 const data = doc.data();
-                // Format the timestamp
                 const createdAt = data.createdAt
                   ? data.createdAt.toDate().toLocaleString()
                   : "";
@@ -43,7 +48,6 @@ export default function Home() {
             );
           });
 
-          // Cleanup the listener on component unmount
           return () => unsubscribe();
         } else {
           setUser(null);
@@ -58,7 +62,30 @@ export default function Home() {
     }
   };
 
-  useEffect(scrollToBottom, [messages]);
+  useEffect(() => {
+    if (messages.length > prevMessageCount) {
+      scrollToBottom();
+      setPrevMessageCount(messages.length);
+    }
+  }, [messages, prevMessageCount]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (chatInputRef.current) {
+        const windowHeight = window.innerHeight;
+        const inputBottom = chatInputRef.current.getBoundingClientRect().bottom;
+        const heightDiff = windowHeight - inputBottom;
+
+        setIsKeyboardOpen(heightDiff < 0);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const signInWithGoogle = async () => {
     try {
@@ -77,12 +104,22 @@ export default function Home() {
 
     const messagesCollection = collection(db, "messages");
 
-    await addDoc(messagesCollection, {
-      text: message,
-      createdAt: serverTimestamp(),
-      uid: user.uid,
-      displayName: user.displayName,
-    });
+    // Generate a unique ID for the message using uuidv4()
+    const messageId = uuidv4();
+
+    const messageDocRef = doc(messagesCollection, messageId);
+
+    try {
+      await setDoc(messageDocRef, {
+        text: message,
+        createdAt: serverTimestamp(),
+        id: messageId,
+        uid: user.uid,
+        displayName: user.displayName,
+      });
+    } catch (error) {
+      console.error("Error adding message document:", error);
+    }
 
     setMessage("");
   };
@@ -90,6 +127,46 @@ export default function Home() {
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       sendMessage();
+    }
+  };
+
+  const toggleReaction = async (reactionType, index) => {
+    if (!user) {
+      return;
+    }
+
+    const message = messages[index];
+
+    if (!message || !message.id) {
+      console.error("Invalid message or message ID:", message);
+      return;
+    }
+
+    const messageId = message.id;
+    const userId = user.uid;
+
+    const reactionUsers = message[`${reactionType}Users`] || [];
+
+    if (reactionUsers.includes(userId)) {
+      const updatedReactionUsers = reactionUsers.filter((id) => id !== userId);
+
+      try {
+        await updateDoc(doc(db, "messages", messageId), {
+          [`${reactionType}Users`]: updatedReactionUsers,
+        });
+      } catch (error) {
+        console.error("Error updating document:", error);
+      }
+    } else {
+      const updatedReactionUsers = [...reactionUsers, userId];
+
+      try {
+        await updateDoc(doc(db, "messages", messageId), {
+          [`${reactionType}Users`]: updatedReactionUsers,
+        });
+      } catch (error) {
+        console.error("Error updating document:", error);
+      }
     }
   };
 
@@ -103,10 +180,72 @@ export default function Home() {
 
           <div className="messages">
             {messages.map((msg, index) => (
-              <div key={index} className="message">
+              <div className="message" key={message.id}>
                 <div className="timestamp">{msg.createdAt}</div>
                 <div className="username">{msg.displayName}</div>
                 <div className="text">{msg.text}</div>
+                <div className="reactions">
+                  <button
+                    className={`reaction-button heart-button ${
+                      msg.heartUsers && msg.heartUsers.includes(user.uid)
+                        ? "clicked"
+                        : ""
+                    }`}
+                    onClick={() => toggleReaction("heart", index)}
+                  >
+                    ❤️ {msg.heartUsers ? msg.heartUsers.length : 0}
+                  </button>
+                  <button
+                    className={`reaction-button like-button ${
+                      msg.likeUsers && msg.likeUsers.includes(user.uid)
+                        ? "clicked"
+                        : ""
+                    }`}
+                    onClick={() => toggleReaction("like", index)}
+                  >
+                    👍 {msg.likeUsers ? msg.likeUsers.length : 0}
+                  </button>
+                  <button
+                    className={`reaction-button dislike-button ${
+                      msg.dislikeUsers && msg.dislikeUsers.includes(user.uid)
+                        ? "clicked"
+                        : ""
+                    }`}
+                    onClick={() => toggleReaction("dislike", index)}
+                  >
+                    👎 {msg.dislikeUsers ? msg.dislikeUsers.length : 0}
+                  </button>
+                  <button
+                    className={`reaction-button laugh-button ${
+                      msg.laughUsers && msg.laughUsers.includes(user.uid)
+                        ? "clicked"
+                        : ""
+                    }`}
+                    onClick={() => toggleReaction("laugh", index)}
+                  >
+                    😂 {msg.laughUsers ? msg.laughUsers.length : 0}
+                  </button>
+                  <button
+                    className={`reaction-button shit-button ${
+                      msg.shitUsers && msg.shitUsers.includes(user.uid)
+                        ? "clicked"
+                        : ""
+                    }`}
+                    onClick={() => toggleReaction("shit", index)}
+                  >
+                    💩 {msg.shitUsers ? msg.shitUsers.length : 0}
+                  </button>
+                  <button
+                    className={`reaction-button potato-button ${
+                      msg.potatoUsers && msg.potatoUsers.includes(user.uid)
+                        ? "clicked"
+                        : ""
+                    }`}
+                    onClick={() => toggleReaction("potato", index)}
+                  >
+                    🥔 {msg.potatoUsers ? msg.potatoUsers.length : 0}
+                  </button>
+                </div>
               </div>
             ))}
             <div ref={messagesEndRef}></div>
